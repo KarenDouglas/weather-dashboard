d434a3c9d489298f4338db487a5673455df2648f
Karen Douglas <kdindependence@gmail.com>
2024-01-09 22:01:23 -0600

    set up api call to 5 day weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 console.log('yo')
+const key = '559b9c3178fdd070bff72b402207d441'
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=30.5083&lon=-97.6789&units=imperial&appid=${key}`)
+.then((response)=>{
+    if(response.ok){
+       return response.json()
+    }
+})
+.then((data)=>{
+    console.log(data.city)
+    console.log(data.list[1].main.temp)
+    console.log(data.list[1].wind)
+    console.log(data.list[1].main.humidity)
+    console.log(data.list[1].weather[0].icon)
+
+}) 
+       
