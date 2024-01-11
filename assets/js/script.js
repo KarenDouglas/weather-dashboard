de6544bd8575250060d8d59b99957da740dfff35
Karen Douglas <kdindependence@gmail.com>
2024-01-11 03:30:02 -0600

    functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,108 @@
+// targeted elements
+const $form = document.querySelector('form')
+const $main = document.querySelector('main')
+const $searchHistory = document.querySelector('#search-history')
+
+// created elements and inner html
+const $forecastWrapper = document.createElement('section')
+$forecastWrapper.classList.add('fourday-forecast-container')
+const $forecastUL = document.createElement('ul')
+$forecastUL.innerHTML = "nothing to see here"
+$forecastUL.classList.add('forecast-container')
+
+
+$forecastWrapper.append($forecastUL)
+
+
+
 const key = '559b9c3178fdd070bff72b402207d441'
-const searchHistory = []
+let searchHistoryArray = JSON.parse(localStorage.getItem('searchHistory')) || []
+const renderSearchHistory = (array) => {
+    $searchHistory.innerHTML = ""
+    if(array.length){
+        for(let i = 0; i < array.length; i++){
+            $searchHistory.innerHTML += `
+            <li class="sh-item" data-city="${array[i]}">${array[i]}</li>           
+    
+            `
+            if(i >= 5){
+                break;
+            }
+        }
+    }else{
+        $searchHistory.innerHTML +='nothing to see here'
+    }
+    
+    
+}
+const renderSearchInfo = (cName,todayDate,temp,wind,humid, icon) => {
+    $main.innerHTML = ""
+    $main.innerHTML +=`   
+    <section class="current-weather-container">
+    <section class="current-weather-city">
+        
+        <h2><i><img src="http://openweathermap.org/img/w/${icon}.png" alt="weather icon"></i>${cName}</h2>
+    </section>
+    <h3>${todayDate}</h3>
+    <hr>
+    <table>
+        <tr>            
+            <td>Temp</td>
+            <td>${temp}</td>
+          </tr>
+          <tr>
+            <td>Wind</td>
+            <td>${wind} MPH</td>
+          </tr>
+          <tr>
+            <td>Humidity</td>
+            <td>${humid}</td>
+          </tr>
+    </table>
+    </section
+    `
+    
+}
+const renderFourDayCards = (data) => {
+    $forecastUL.innerHTML = ""
+    const indexArray = [0,7,15,23,31]
+    for(let i = 0; i < indexArray.length; i++){
+        const date = data.list[indexArray[i]].dt_txt.split(' ')[0]
+        const temp = data.list[indexArray[i]].main.temp
+        const wind = data.list[indexArray[i]].wind.speed
+        const humid = data.list[indexArray[i]].main.humidity
+        const icon = data.list[indexArray[i]].weather[0].icon
 
-
+        $forecastUL.innerHTML += `
+        <li class="fdfc-item">
+        <div>
+            <ul class="day-item">
+                <li class="date">
+                    <span>${date}</span>
+                </li>
+                <li class="icon">
+                    <span><img src="http://openweathermap.org/img/w/${icon}.png" alt="weather icon"></span>
+                </li>
+                <li>
+                    <span class="title">Temp</span>
+                    <span>${temp} °F</span>
+                </li>
+                <li>
+                    <span class="title">Wind</span>
+                    <span>${wind} MPH</span>
+                </li>
+                <li>
+                    <span class="title">Humidity</span>
+                    <span>${humid} %</span>
+                </li>
+            </ul>
+        </div>
+    </li>
+        `
+    console.log('here')
+    
+    } 
+}
 // fetches weather data for city
 const fetchDataByLatLon = (lat, lon) => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`)
@@ -11,7 +112,9 @@ const fetchDataByLatLon = (lat, lon) => {
         }
     })
     .then((data)=>{
-        
+        const cityName = data.city.name
+        const dateDUMMY = `Jan 11, 2024`
+       
         // console.log(data.city.name) 
         // console.log(data.list[1].main.temp)
         // console.log(data.list[1].wind.speed)
@@ -22,7 +125,12 @@ const fetchDataByLatLon = (lat, lon) => {
         const thirdDay = 15
         const fourthDay = 23
         const firstDayDay = 31
-        console.log(data.list[31])
+        renderSearchInfo(cityName,dateDUMMY,data.list[0].main.temp, data.list[0].wind.speed, data.list[0].main.humidity, data.list[0].weather[0].icon)
+       console.log(data.list[0].dt_txt.split(' ')[0])      
+       console.log($main)
+       $main.append($forecastWrapper)
+       renderFourDayCards(data)
+       
         
     }) 
     
@@ -44,6 +152,26 @@ const convertCityNameToCoord = (cityName) => {
     
 }
 
+const handleSearch = (e) => {
+    const input = document.querySelector('input')
+    searchHistoryArray = JSON.parse(localStorage.getItem('searchHistory')) || []
+ 
+    e.preventDefault()
+    convertCityNameToCoord(input.value)
+    searchHistoryArray.unshift(input.value)
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistoryArray))
+    renderSearchHistory(searchHistoryArray)
+}
 
+const handleHistoryLink = (e) => {
+    if(e.target.hasAttribute('data-city')){
+        convertCityNameToCoord(e.target.dataset.city)
+    }
+    console.log('after')
+}
+//convertCityNameToCoord('round rock')
+renderSearchHistory(searchHistoryArray)
+$form.addEventListener('submit', handleSearch)
+$searchHistory.addEventListener('click', handleHistoryLink)
 
        
