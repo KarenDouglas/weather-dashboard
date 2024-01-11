44ab810fce21456f34e3c4c23d75db553c6e396b
Karen Douglas <kdindependence@gmail.com>
2024-01-10 23:28:09 -0600

    styling layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,49 @@
-console.log('yo')
 const key = '559b9c3178fdd070bff72b402207d441'
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=30.5083&lon=-97.6789&units=imperial&appid=${key}`)
-.then((response)=>{
-    if(response.ok){
-       return response.json()
-    }
-})
-.then((data)=>{
-    console.log(data.city)
-    console.log(data.list[1].main.temp)
-    console.log(data.list[1].wind)
-    console.log(data.list[1].main.humidity)
-    console.log(data.list[1].weather[0].icon)
+const searchHistory = []
 
-}) 
+
+// fetches weather data for city
+const fetchDataByLatLon = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`)
+    .then((response)=>{
+        if(response.ok){
+           return response.json()
+        }
+    })
+    .then((data)=>{
+        
+        // console.log(data.city.name) 
+        // console.log(data.list[1].main.temp)
+        // console.log(data.list[1].wind.speed)
+        // console.log(data.list[1].main.humidity)
+        // console.log(data.list[1].weather[0].icon)
+        const firstDay = 0
+        const secondDay = 7
+        const thirdDay = 15
+        const fourthDay = 23
+        const firstDayDay = 31
+        console.log(data.list[31])
+        
+    }) 
+    
+}
+// converts coordinate into it's city name
+const convertCityNameToCoord = (cityName) => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`)
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }
+    })
+    .then((data)=>{
+       const lat = data.city.coord.lat 
+       const lon = data.city.coord.lon 
+    console.log('geo',lat,lon)
+    fetchDataByLatLon(lat,lon)
+    })
+    
+}
+
+
+
        
